b4c9d8c24856c4d7249d8f8b601f880eeb43465b
YEJIN-LILY <57944153+YEJIN-LILY@users.noreply.github.com>
2020-12-01 12:28:19 +0900

    Update pi_detect_drowsiness.py

--- pi_detect_drowsiness.py
+++ pi_detect_drowsiness.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from picamera import PiCamera
-from imutils.video import VideoStream
 from imutils import face_utils
 import numpy as np
 import argparse
@@ -11,14 +9,16 @@ import dlib
 import cv2
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
-# 피에조 번호로 바꿔주세요
-led_pin1=14
-led_pin2=15
-
-GPIO.setup(led_pin1,GPIO.OUT)
-GPIO.setup(led_pin2,GPIO.OUT)
-
+# 피에조 번호 할당
+gpio_pin=13
+scale = [261, 392]
+GPIO.setup(gpio_pin,GPIO.OUT)
+#gpio_pin의 주파수 100인 pwm 인스턴스 생성
+# duty cycle을 100%로 시작
+p = GPIO.PWM(gpio_pin, 100)
+GPIO.output(gpio_pin,True)
 
 def euclidean_dist(ptA, ptB):
 	# 두 점 사이의 거리 리턴
@@ -41,7 +41,7 @@ try:
 	# ap.add_argument("-a", "--alarm", type=int, default=0,help="boolean used to indicate if TrafficHat should be used")
 	args = vars(ap.parse_args())
 	
-	# threshold 지정
+	# 임계값 지정
 	EYE_AR_THRESH = 0.3
 	EYE_AR_CONSEC_FRAMES = 16
 	
@@ -63,80 +63,82 @@ try:
 		exit()
 	time.sleep(1.0)
 	# ---------------------------------------------------------------------
-	# loop over frames from the video stream
+	# 비디오 실행중 프레임 반복
 	while True:
-		# grab the frame from the threaded video file stream, resize
-		# it, and convert it to grayscale
-		# channels)
+                
+		# 프레임을 가져오고 크기 조정
+		# 그레이스케일(회색조)로 변환(효율성)
 		ret,frame = vs.read()
 		frame = imutils.resize(frame, width=450)
 		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		# detect faces in the grayscale frame
+		
+		# 그레이 스케일 이미지에서 얼굴 감지
 		rects = detector.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30),flags=cv2.CASCADE_SCALE_IMAGE)
 		
-		# loop over the face detections
+		# 얼굴 감지 반복
 		for (x, y, w, h) in rects:
-			# construct a dlib rectangle object from the Haar cascade
-			# bounding box
+                        
+			# haar cascade에서 dlib 직사각형 객체 생성
 			rect = dlib.rectangle(int(x), int(y), int(x + w),int(y + h))
-			# determine the facial landmarks for the face region, then
-			# convert the facial landmark (x, y)-coordinates to a NumPy
-			# array
+			
+			# 얼굴 영역 얼굴 landmarks 결정
+			# 얼굴 landmark (x,y)좌표를 Numpy 배열로 변환
 			shape = predictor(gray, rect)
 			shape = face_utils.shape_to_np(shape)
 			
-			# extract the left and right eye coordinates, then use the
-			# coordinates to compute the eye aspect ratio for both eyes
+			# 배열이 주어지면 눈 좌표 추출
+			# 양쪽 눈 종횡비(ear값) 계산하는 좌표
 			leftEye = shape[lStart:lEnd]
 			rightEye = shape[rStart:rEnd]
 			leftEAR = eye_aspect_ratio(leftEye)
 			rightEAR = eye_aspect_ratio(rightEye)
-			# average the eye aspect ratio together for both eyes
-			ear = (leftEAR + rightEAR) / 2.0
 			
-			# compute the convex hull for the left and right eye, then
-			# visualize each of the eyes
+			# 양쪽 눈 종횡비(ear값) 평균 구하기 (더 정확한 추정치)
+			ear = (leftEAR + rightEAR) / 2.0
+
+			# 양쪽 눈 볼록껍질 계산
+			# drawContours 함수 이용해 실시간으로 눈 주변에 선 그리기
 			leftEyeHull = cv2.convexHull(leftEye)
 			rightEyeHull = cv2.convexHull(rightEye)
 			cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
 			cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
 			# ---------------------------------------------------------------------
-			# check to see if the eye aspect ratio is below the blink
-			# threshold, and if so, increment the blink frame counter
+			# 눈 종횡비가 깜박임보다 작으면
+			# 임계값(0.3)보다 작으면 카운터를 증가시킨다
 			if ear < EYE_AR_THRESH:
 				COUNTER += 1
-				# if the eyes were closed for a sufficient number of
-				# frames, then sound the alarm
+				# 카운터가 임계값(16)보다 크면 
+				# 알람을 울린다
+				
 				if COUNTER >= EYE_AR_CONSEC_FRAMES:
-					# if the alarm is not on, turn it on
-					if not ALARM_ON:
-						ALARM_ON = True
-						# check to see if the TrafficHat buzzer should
-						# be sounded
-						#if args["alarm"] > 0: # 알람 파라미터 안주었습니다~이 부분 피에조로 구현해주세요
-							#th.buzzer.blink(0.1, 0.1, 10,background=True)
-							
-					# draw an alarm on the frame
-					cv2.putText(frame, "DROWSINESS ALERT!", (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-			# otherwise, the eye aspect ratio is not below the blink
-			# threshold, so reset the counter and alarm
+					p.start(100)
+					p.ChangeDutyCycle(90)
+					p.ChangeFrequency(scale[0])
+					time.sleep(1)
+					cv2.putText(frame, "ALAM ON!", (10, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+				else:
+					p.stop()
+
+			# 눈 가로 세로 비율이 깜박임보다 크면
+			# 카운터와 알람을 reset한다
 			else:
+				p.stop()
 				COUNTER = 0
 				ALARM_ON = False
 			
-			# draw the computed eye aspect ratio on the frame to help
-			# with debugging and setting the correct eye aspect ratio
-			# thresholds and frame counters
+			# 눈 가로 세로 비율을 보여준다
+			# 디버깅과 적절한 눈 가로 세로 비율 설정(300,30)
+			# 임계값, 프레임, 카운터
 			cv2.putText(frame, "EAR: {:.3f}".format(ear), (300, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 	
-		# show the frame
+		# 프레임을 보여준다
 		cv2.imshow("Frame", frame)
 		key = cv2.waitKey(1) & 0xFF
  
-		# if the `q` key was pressed, break from the loop
+		# 'q'를 누르면 loop를 멈춘다
 		if key == ord("q"):
 			break
-	# do a bit of cleanup
+	# 윈도우 종료
 	cv2.destroyAllWindows()
 finally:
 	print("Cleaning up")
